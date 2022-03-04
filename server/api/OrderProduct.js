dd3083076629ff2beb7158c89e83cb0d42f37515
Robert Berman <robertberman.tech@gmail.com>
2022-03-04 16:50:25 -0500

    created add and find routes and thunks for orders and their items

--- server/api/OrderProduct.js
+++ server/api/OrderProduct.js
@@ -4,6 +4,20 @@ const {
 } = require('../db');
 module.exports = router;
 
+// getting the user's current cart items that match the order Id
+router.get('/:orderId', async (req, res, next) => {
+  try {
+    const cart = await OrderProduct.findAll({
+      where: {
+        orderId: req.params.orderId,
+      },
+    });
+    res.json(cart);
+  } catch (err) {
+    next(err);
+  }
+});
+
 router.post('/', async (req, res, next) => {
   try {
     const cartItem = await OrderProduct.create(req.body);
--- server/api/cart.js
+++ server/api/cart.js
@@ -4,10 +4,25 @@ const {
 } = require('../db');
 module.exports = router;
 
+// getting the user's current cart that isn't fulfilled
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const cart = await Order.findOne({
+      where: {
+        userId: req.params.userId,
+        isFulfilled: false,
+      },
+    });
+    res.json(cart);
+  } catch (err) {
+    next(err);
+  }
+});
+
 // posting a card to order models
 router.post('/', async (req, res, next) => {
   try {
-    const cart = await Order.create(req.body);
+    const cart = await Order.create(req.body.userId);
     res.json(cart);
   } catch (err) {
     next(err);
