bb150b94f901d7e5084bd9fd69795ea89376a3ca
Robert Berman <robertberman.tech@gmail.com>
2022-03-06 17:11:30 -0500

    removed casing inconsistencies with axios definitions

--- client/store/SingleProduct.js
+++ client/store/SingleProduct.js
@@ -1,6 +1,6 @@
-import Axios from "axios";
+import axios from 'axios';
 //action creator
-const SET_SINGLE_PRODUCT = "SET_SINGLE_PRODUCT";
+const SET_SINGLE_PRODUCT = 'SET_SINGLE_PRODUCT';
 
 // action creator
 export const setSingleProduct = (product) => {
@@ -15,9 +15,8 @@ export const setSingleProduct = (product) => {
 export const fetchSingleProduct = (id) => {
   return async (dispatch) => {
     try {
-      const { data } = await Axios.get(`/api/products/${id}`);
+      const { data } = await axios.get(`/api/products/${id}`);
       dispatch(setSingleProduct(data));
-
     } catch (err) {
       console.log(err);
     }
@@ -27,8 +26,7 @@ export const fetchSingleProduct = (id) => {
 export default function productReducer(state = {}, action) {
   switch (action.type) {
     case SET_SINGLE_PRODUCT:
-
-      console.log("here is action", action);
+      console.log('here is action', action);
       return { ...action.product };
     default:
       return state;
